a19f80b9c1148474b3b8fb54c1eb9ca0834abb1a
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-05-19 15:23:35 -0500

    test

--- main.go
+++ main.go
@@ -2,32 +2,14 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"context"
+	"github.com/sgapnayr/golang-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Get("/hello", basicHandler)
-
-	addr := "localhost:3000"
-	
-	server := &http.Server{
-		Addr:    addr,
-		Handler: router,
-	}
-
-	fmt.Printf("Starting server on %s\n", addr)
-	err := server.ListenAndServe()
+	app := application.New()
+	err := app.Start(context.TODO())
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		fmt.Println("Error starting server: ", err)
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
 }
