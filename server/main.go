9d9d1a4d13b3f6dae4d4bfcd65d695aa9a5b6c02
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-06-05 13:44:28 +0300

    CI/CD Update

--- server/main.go
+++ server/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -43,8 +49,34 @@ func main() {
 	r.PUT("/orders/:id/:item", updateOrderHandler)
 	r.DELETE("/orders/:id/:item", deleteOrderHandler)
 
-	// Start the server
-	r.Run(":8080")
+	// Create an HTTP server
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	// Start the server in a goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	log.Println("Server started on :8080")
+
+	// Wait for interrupt signal to gracefully shutdown the server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Create a context with a timeout to allow outstanding requests to finish
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
 
 // Handlers
